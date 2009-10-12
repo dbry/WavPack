9ff01cfcc79858cb3e4429310dba817c668877d8
Sebastian Dröge <slomo@circular-chaos.org>
2009-10-12 05:51:47 +0000

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,41 +1,12 @@
 #!/bin/sh
 
-DIE=0
-PACKAGE=wavpack
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-echo "Generating configuration files for $PACKAGE, please wait..."
+ORIGDIR=`pwd`
+cd $srcdir
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PACKAGE."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
+autoreconf -v --install || exit 1
+cd $ORIGDIR || exit $?
 
-for LIBTOOLIZE in libtoolize glibtoolize nope; do
-	$LIBTOOLIZE --version > /dev/null 2>&1 && break
-done
-if test x$LIBTOOLIZE = xnope; then
-	echo
-	echo "You must have libtool installed to compile $PACKAGE."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-fi
-
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PACKAGE."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-[ $DIE -eq 1 ] && exit 1
-
-touch NEWS README AUTHORS ChangeLog
-aclocal
-$LIBTOOLIZE --copy --force
-automake --copy --add-missing --force
-autoconf --force
+$srcdir/configure --enable-maintainer-mode --enable-man "$@"
