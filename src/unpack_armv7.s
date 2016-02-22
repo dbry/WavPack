89f1d41f3e8eefa215ddcee657fcc8e32415490f
David Bryant <david@wavpack.com>
2016-02-21 20:30:09 -0800

    fix syntax issue in ARM assembly file

--- src/unpack_armv7.s
+++ src/unpack_armv7.s
@@ -6,7 +6,7 @@
 @@      Distributed under the BSD Software License (see license.txt)      @@
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-        .section .note.GNU-stack,"",@progbits
+        .section .note.GNU-stack,"",%progbits
         .text
         .align
         .global         unpack_decorr_stereo_pass_cont_armv7
