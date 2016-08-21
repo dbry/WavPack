07bb03880997c3de12ea9611a6342f8bf86c1fba
David Bryant <david@wavpack.com>
2016-08-21 11:57:15 -0700

    fix compile issue (VS2013) reported on HA (thanks Spoon!)

--- src/wavpack_local.h
+++ src/wavpack_local.h
@@ -717,7 +717,7 @@ uint32_t bs_close_read (Bitstream *bs);
 #ifdef HAVE___BUILTIN_CLZ
 #define count_bits(av) ((av) ? 32 - __builtin_clz (av) : 0)
 #elif defined (_WIN64)
-static __inline int count_bits (uint32_t av) { long res; return _BitScanReverse (&res, av) ? (int)(res + 1) : 0; }
+static __inline int count_bits (uint32_t av) { unsigned long res; return _BitScanReverse (&res, av) ? (int)(res + 1) : 0; }
 #else
 #define count_bits(av) ( \
  (av) < (1 << 8) ? nbits_table [av] : \
