dfc67c425ce8200acdb04b81dc5dc9a28dea5414
tico-tico <sergei.ivn@gmx.com>
2018-12-21 01:27:43 +0300

    restrict _BitScanForward to msvc

--- src/read_words.c
+++ src/read_words.c
@@ -124,7 +124,7 @@ int32_t FASTCALL get_word (WavpackStream *wps, int chan, int32_t *correction)
             wps->wvbits.bc += sizeof (*(wps->wvbits.ptr)) * 8;
         }
 
-#ifdef _WIN32
+#ifdef _MSC_VER
         { unsigned long res; _BitScanForward (&res, (unsigned long)~wps->wvbits.sr); ones_count = (uint32_t) res; }
 #else
         ones_count = __builtin_ctz (~wps->wvbits.sr);
@@ -403,7 +403,7 @@ int32_t get_words_lossless (WavpackStream *wps, int32_t *buffer, int32_t nsample
             bs->bc += sizeof (*(bs->ptr)) * 8;
         }
 
-#ifdef _WIN32
+#ifdef _MSC_VER
         { unsigned long res; _BitScanForward (&res, (unsigned long)~wps->wvbits.sr); ones_count = (uint32_t) res; }
 #else
         ones_count = __builtin_ctz (~wps->wvbits.sr);
