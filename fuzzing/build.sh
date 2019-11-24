83fe0d9819c21a87953b637264dc897929cc35c5
David Bryant <david@wavpack.com>
2019-11-24 10:17:29 -0800

    add initial fuzzing files from Thuan

---
+++ fuzzing/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build project
+# e.g.
+./autogen.sh
+./configure
+make
+
+# build fuzzers
+# e.g.
+$CXX $CXXFLAGS -std=c++11 -I$SRC/wavpack/include -I$SRC/wavpack/cli \
+     $SRC/wavpack/fuzzing/fuzzer.cc -o $OUT/fuzzer \
+     $LIB_FUZZING_ENGINE $SRC/wavpack/src/.libs/libwavpack.a
+
+# add seed corpus
+cp $SRC/wavpack/fuzzing/*_seed_corpus.zip $OUT/
+
+# add dictionary
+cp $SRC/wavpack/fuzzing/*.dict $SRC/wavpack/fuzzing/*.options $OUT/
---
+++ fuzzing/fuzzer.cc
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <vector>
+#include <fstream>
+
+#include "wavpack.h"
+#include "md5.h"
+
+using namespace std;
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    //WavpackContext *wpc = WavpackOpenRawDecoder((void *)data, size, 0, 0, 0, 0, 0, 0);
+    ofstream myFile ("/tmp/data.bin", ios::out | ios::binary);
+    
+    myFile.write ((char*)data, size);
+    char error [80];
+    WavpackContext *wpc = WavpackOpenFileInput ("/tmp/data.bin", error, OPEN_WVC | OPEN_DSD_NATIVE | OPEN_ALT_TYPES, 0);
+
+    char md5_string1 [] = "????????????????????????????????";
+    char md5_string2 [] = "????????????????????????????????";
+    int32_t *decoded_samples, num_chans, bps, test_index, qmode, total_samples;
+    unsigned char md5_initial [16], md5_stored [16];
+    MD5_CTX md5_global, md5_local;
+    unsigned char *chunked_md5;
+
+    if (!wpc) return 1;
+
+    num_chans = WavpackGetNumChannels (wpc);
+    total_samples = WavpackGetNumSamples64 (wpc);
+    bps = WavpackGetBytesPerSample (wpc);
+    qmode = WavpackGetQualifyMode (wpc);
+
+    WavpackCloseFile (wpc);
+
+    return 0;
+}
