0a45e2b22fd64702443c875c1ac87b89265a9662
David Bryant <david@wavpack.com>
2019-11-30 12:53:09 -0800

    remove C++ compilation fix because it broke big-endian builds (and this is C)

--- src/open_utils.c
+++ src/open_utils.c
@@ -808,8 +808,8 @@ static void bs_read (Bitstream *bs);
 static void bs_open_read (Bitstream *bs, void *buffer_start, void *buffer_end)
 {
     bs->error = bs->sr = bs->bc = 0;
-    bs->ptr = ((bs->buf = (uint16_t *)buffer_start) - 1);
-    bs->end = (uint16_t *)buffer_end;
+    bs->ptr = (bs->buf = buffer_start) - 1;
+    bs->end = buffer_end;
     bs->wrap = bs_read;
 }
 
