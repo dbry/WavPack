109a35ecb400580983790961c9066bb1449185db
David Bryant <david@wavpack.com>
2020-01-12 10:41:36 -0800

    add directory of regression tests that used to crash the sanitizer enhance the stand-alone fuzzing test jig to handle multiple files call more libwavpack APIs from the fuzzer.cc to improve coverage

--- fuzzing/fuzzer.cc
+++ fuzzing/fuzzer.cc
@@ -49,7 +49,7 @@ static int32_t raw_read_bytes (void *id, void *data, int32_t bcount)
 
 static int32_t raw_write_bytes (void *id, void *data, int32_t bcount)
 {
-    return 0;
+    return data ? bcount : 0;
 }
 
 static int64_t raw_get_pos (void *id)
@@ -117,8 +117,11 @@ static WavpackStreamReader64 raw_reader = {
     raw_push_back_byte, raw_get_length, raw_can_seek, NULL, raw_close_stream
 };
 
+static long long debug_log_mask = -1;
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-    static long long times_called, opens, seeks, samples_decoded, text_tags, binary_tags;
+    static long long times_called, opens, seeks, tag_writes, samples_decoded, text_tags, binary_tags;
+    int flags = OPEN_TAGS | OPEN_EDIT_TAGS | OPEN_WRAPPER | OPEN_DSD_AS_PCM | OPEN_NO_CHECKSUM | OPEN_NORMALIZE;
     WavpackRawContext raw_wv;
     WavpackContext *wpc;
     char error [80];
@@ -128,10 +131,13 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
     times_called++;
 
+    WavpackGetLibraryVersionString ();
+    WavpackGetLibraryVersion ();
+
     memset (&raw_wv, 0, sizeof (WavpackRawContext));
     raw_wv.dptr = raw_wv.sptr = (unsigned char *) data;
     raw_wv.eptr = raw_wv.dptr + size;
-    wpc = WavpackOpenFileInputEx64 (&raw_reader, &raw_wv, NULL, error, OPEN_TAGS | OPEN_WRAPPER | OPEN_DSD_AS_PCM | OPEN_NO_CHECKSUM, 0);
+    wpc = WavpackOpenFileInputEx64 (&raw_reader, &raw_wv, NULL, error, flags, 15);
 
     if (!wpc) {
         retval = 1;
@@ -144,6 +150,41 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     bps = WavpackGetBytesPerSample (wpc);
     qmode = WavpackGetQualifyMode (wpc);
     mode = WavpackGetMode (wpc);
+
+    // call some other APIs for coverage
+    WavpackGetErrorMessage (wpc);
+    WavpackGetSampleIndex64 (wpc);
+    WavpackGetSampleIndex (wpc);
+    WavpackGetNumSamples (wpc);
+    WavpackGetNumErrors (wpc);
+    WavpackLossyBlocks (wpc);
+    WavpackGetProgress (wpc);
+    WavpackGetRatio (wpc);
+    WavpackGetAverageBitrate (wpc, 1);
+    WavpackGetInstantBitrate (wpc);
+    WavpackGetNativeSampleRate (wpc);
+    WavpackGetSampleRate (wpc);
+    WavpackGetChannelMask (wpc);
+    WavpackGetFloatNormExp (wpc);
+    WavpackGetBitsPerSample (wpc);
+    WavpackGetBytesPerSample (wpc);
+    WavpackGetReducedChannels (wpc);
+    WavpackGetVersion (wpc);
+    WavpackGetFileFormat (wpc);
+    WavpackGetFileExtension (wpc);
+
+    if (WavpackGetWrapperBytes (wpc))
+        WavpackGetWrapperData (wpc);
+
+    if (num_chans) {
+        unsigned char identities [num_chans + 1];
+        WavpackGetChannelIdentities (wpc, identities);
+
+        if (WavpackGetChannelLayout (wpc, NULL) & 0xff) {
+            unsigned char reordering [WavpackGetChannelLayout (wpc, NULL) & 0xff];
+            WavpackGetChannelLayout (wpc, reordering);
+        }
+    }
 
     // Get all the metadata tags (text & binary)
     if (mode & MODE_VALID_TAG) {
@@ -179,29 +220,42 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
             free (value);
             free (item);
         }
+
+        WavpackAppendTagItem (wpc, "Artist", "The Googlers", strlen ("The Googlers"));
+        WavpackAppendTagItem (wpc, "Title", "Fuzz Me All Night Long", strlen ("Fuzz Me All Night Long"));
+        WavpackAppendTagItem (wpc, "Album", "Meet The Googlers", strlen ("Meet The Googlers"));
+        WavpackAppendBinaryTagItem (wpc, "Cover Art (Front)", (const char *) data, size < 4096 ? size : 4096);
     }
 
     // Decode all
     if (num_chans && num_chans <= 256) {
         int32_t decoded_samples [BUF_SAMPLES * num_chans];
+        unsigned char md5sum [16];
         int unpack_result;
 
         do {
             unpack_result = WavpackUnpackSamples (wpc, decoded_samples, BUF_SAMPLES);
             samples_decoded += unpack_result;
         } while (unpack_result);
+
+        WavpackGetMD5Sum (wpc, md5sum);
     }
 
     // Seek to 1/3 of the way in plus 1000 samples (definitely not a block boundary)
-    if (WavpackSeekSample64 (wpc, total_samples / 3 + 1000))
+    if (WavpackSeekSample64 (wpc, total_samples / 3 + 1000)) {
         ++seeks;
+
+        // if we're still okay, try to write out the modified tags
+        if (WavpackWriteTag (wpc))
+            ++tag_writes;
+    }
 
     WavpackCloseFile (wpc);
 
 exit:
-    if (!(times_called | 0x3FF))
-        printf ("LLVMFuzzerTestOneInput(): %lld calls, %lld opens, %lld seeks, %lld samples, %lld text & %lld binary tags\n",
-            times_called, opens, seeks, samples_decoded, text_tags, binary_tags);
+    if (!(times_called & debug_log_mask))
+        printf ("LLVMFuzzerTestOneInput(): %lld calls, %lld opens, %lld seeks, %lld tag writes, %lld samples, %lld text & %lld binary tags\n",
+            times_called, opens, seeks, tag_writes, samples_decoded, text_tags, binary_tags);
 
     return retval;
 }
@@ -211,37 +265,29 @@ exit:
 int main (int argc, char **argv)
 {
     unsigned char *buffer = (unsigned char *) malloc (STAND_ALONE_LENGTH);
-    const char *filename = argc > 1 ? argv [1] : "testfile.wv";
-    FILE *infile = fopen (filename, "rb");
-    int retval, count = 1;
-    int bytes_read;
+    int index;
 
-    if (!infile) {
-        fprintf (stderr, "can't open file %s!\n", filename);
-        return 1;
-    }
+    // debug_log_mask = 0;
 
-    if (argc > 2) {
-        count = atol (argv [2]);
+    for (index = 1; index < argc; ++index) {
+        const char *filename = argv [index];
+        FILE *infile = fopen (filename, "rb");
+        int bytes_read;
 
-        if (!count) {
-            fprintf (stderr, "second parameter is count!\n");
-            return 1;
+        if (!infile) {
+            fprintf (stderr, "can't open file %s!\n", filename);
+            continue;
         }
+
+        bytes_read = fread (buffer, 1, STAND_ALONE_LENGTH, infile);
+        printf ("read %d bytes from file %s\n", bytes_read, filename);
+
+        if (bytes_read == STAND_ALONE_LENGTH)
+            printf ("warning: at maximum length, perhaps truncated!\n");
+
+        fclose (infile);
+        LLVMFuzzerTestOneInput(buffer, bytes_read);
     }
-
-    bytes_read = fread (buffer, 1, STAND_ALONE_LENGTH, infile);
-    printf ("read %d bytes from file %s, process count = %d\n", bytes_read, filename, count);
-
-    if (bytes_read == STAND_ALONE_LENGTH)
-        printf ("warning: at maximum length, perhaps truncated!\n");
-
-    fclose (infile);
-
-    while (count--)
-        retval = LLVMFuzzerTestOneInput(buffer, bytes_read);
-
-    printf ("retval = %d\n", retval);
 
     free (buffer);
 
