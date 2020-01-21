4919220b93002a19276d3e7bbe1a8ec8ceb8e132
David Bryant <david@wavpack.com>
2020-01-20 16:01:11 -0800

    two more undefined behavior fixes and one more regression crasher

--- src/open_utils.c
+++ src/open_utils.c
@@ -507,6 +507,16 @@ static int read_channel_info (WavpackContext *wpc, WavpackMetadata *wpmd)
 
 static int read_channel_identities (WavpackContext *wpc, WavpackMetadata *wpmd)
 {
+    unsigned char *idents = wpmd->data;
+    int i;
+
+    if (!wpmd->data || !wpmd->byte_length)
+        return FALSE;
+
+    for (i = 0; i < wpmd->byte_length; ++i)
+        if (!idents [i])
+            return FALSE;
+
     if (!wpc->channel_identities) {
         wpc->channel_identities = (unsigned char *)malloc (wpmd->byte_length + 1);
         memcpy (wpc->channel_identities, wpmd->data, wpmd->byte_length);
