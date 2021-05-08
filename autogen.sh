9a51024cfda2916ca887a49e7c33efb57f4d358f
David Seifert <soap@gentoo.org>
2021-05-09 00:02:32 +0200

    Clean up Autotools

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,8 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+test -f "config.rpath" || cp /usr/share/gettext/config.rpath . 2>/dev/null || touch config.rpath || exit 1
+
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
