89ef99e84333534d9d43093a5264a398b5f1e14a
David Bryant <david@wavpack.com>
2022-07-07 15:55:09 -0700

    fix length update in library-generated WAV headers on big-endian machines

--- src/pack_utils.c
+++ src/pack_utils.c
@@ -1057,9 +1057,6 @@ void WavpackUpdateNumSamples (WavpackContext *wpc, void *first_block)
 {
     uint32_t wrapper_size;
 
-    WavpackLittleEndianToNative (first_block, WavpackHeaderFormat);
-    SET_TOTAL_SAMPLES (* (WavpackHeader *) first_block, WavpackGetSampleIndex64 (wpc));
-
     if (wpc->riff_header_created && WavpackGetWrapperLocation (first_block, &wrapper_size)) {
         unsigned char riff_header [128];
 
@@ -1067,6 +1064,8 @@ void WavpackUpdateNumSamples (WavpackContext *wpc, void *first_block)
             memcpy (WavpackGetWrapperLocation (first_block, NULL), riff_header, wrapper_size);
     }
 
+    WavpackLittleEndianToNative (first_block, WavpackHeaderFormat);
+    SET_TOTAL_SAMPLES (* (WavpackHeader *) first_block, WavpackGetSampleIndex64 (wpc));
     block_update_checksum (first_block);
     WavpackNativeToLittleEndian (first_block, WavpackHeaderFormat);
 }
