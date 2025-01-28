336ca0b6c25955f4e87f1633746bb79881a2bf1e
Caleb Xu <calebcenter@live.com>
2025-01-28 01:32:27 -0500

    cli: support detecting cores in macOS and other BSDs
    - follow-up to 8b5b1f54863c2b3b2d578d3c4c8434a7447134e5
    - macOS and BSDs do not have sys/sysinfo.h or get_nprocs(3)
    - on macOS and BSDs, use sysctl(3) in sys/sysctl.h instead

--- cli/utils.c
+++ cli/utils.c
@@ -671,7 +671,9 @@ int check_break (void)
 
 #ifdef ENABLE_THREADS
 
-#if defined(__GNUC__) && !defined(_WIN32)
+#if defined(__APPLE__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+#include <sys/sysctl.h>
+#elif defined(__GNUC__) && !defined(_WIN32)
 #include <sys/sysinfo.h>
 #endif
 
@@ -683,6 +685,10 @@ int get_default_worker_threads (void)
     SYSTEM_INFO sysinfo;
     GetSystemInfo (&sysinfo);
     num_processors = sysinfo.dwNumberOfProcessors;
+#elif defined(__APPLE__) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+    size_t len = sizeof (num_processors);
+    int mib[2] = { CTL_HW, HW_NCPU };
+    sysctl (mib, 2, &num_processors, &len, NULL, 0);
 #elif defined (__GNUC__)
     num_processors = get_nprocs ();
 #endif
